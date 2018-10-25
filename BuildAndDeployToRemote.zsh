72534a19c5290da2ca2e06fbb2e78a9fb219f0ba
sreekar <sreekar339@gmail.com>
2018-10-26 04:35:11 +0530

    .service file, deploy file, error in config file

--- BuildAndDeployToRemote.zsh
+++ BuildAndDeployToRemote.zsh
@@ -1,9 +1,27 @@
+#!/bash/zsh
 echo "deployToRemote script started running\n"
 
-GOOS=linux GOARCH=amd64 go build .
-
-ssh -t sreekar339@139.59.93.218 'sudo systemctl stop GoFileServer'
-
-scp GoFileServer ServerConfig.yml sreekar339@139.59.93.218:~/backend/go/fileServer/
-
-ssh -t sreekar339@139.59.93.218 'sudo systemctl start GoFileServer'
+# rm lol.lol \
+# \
+GOOS=linux GOARCH=amd64 go build . \
+&& echo "0" \
+&& scp GoFileServer.service sreekar339@139.59.93.218:~/  \
+&& echo "1" \
+&& ssh -t sreekar339@139.59.93.218 'sudo systemctl stop GoFileServer' \
+&& echo "2" \
+&& scp GoFileServer ServerConfig.yml sreekar339@139.59.93.218:~/backend/go/fileServer \
+&& echo "3" \
+&& ssh -t sreekar339@139.59.93.218 '
+echo "4";
+sudo rm /etc/systemd/system/GoFileServer.service;
+echo "5";
+sudo mv ~/GoFileServer.service /etc/systemd/system/GoFileServer.service;
+echo "6";
+sudo systemctl daemon-reload &&
+echo "7" &&
+sudo systemctl enable GoFileServer &&
+echo "8" &&
+sudo systemctl start GoFileServer &&
+echo "9"
+' \
+&& echo "10"
