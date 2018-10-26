22401d0adae227df48c2df7e895ad69e8ae97525
sreekar <sreekar339@gmail.com>
2018-10-26 20:40:00 +0530

    need a lot of fixing in deploy script

--- BuildAndDeployToRemote.zsh
+++ BuildAndDeployToRemote.zsh
@@ -1,29 +1,30 @@
 #!/bash/zsh
 echo "\ndeployToRemote script started running\n"
 
-# rm lol.lol \
+# rm lol.lol 
 # \
-GOOS=linux GOARCH=amd64 go build . \
-&& echo "0" \
-&& scp GoFileServer.service sreekar339@139.59.93.218:~/  \
-&& echo "1" \
-&& ssh -t sreekar339@139.59.93.218 'sudo systemctl stop GoFileServer' \
-&& echo "2" \
-&& scp GoFileServer ServerConfig.yml sreekar339@139.59.93.218:~/backend/go/fileServer \
-&& echo "3" \
-&& ssh -t sreekar339@139.59.93.218 '
-echo "4";
-sudo rm /etc/systemd/system/GoFileServer.service;
-echo "5";
-sudo mv ~/GoFileServer.service /etc/systemd/system/GoFileServer.service;
-echo "6";
-sudo systemctl daemon-reload &&
-echo "7" &&
-sudo systemctl enable GoFileServer &&
-echo "8" &&
-sudo systemctl start GoFileServer &&
+GOOS=linux GOARCH=amd64 go build .
+echo "0"
+scp GoFileServer.service sreekar339@139.59.93.218:~/
+echo "1" 
+ssh -t sreekar339@139.59.93.218 'sudo systemctl stop GoFileServer' 
+echo "2" 
+scp GoFileServer ServerConfig.yml sreekar339@139.59.93.218:~/backend/go/fileServer 
+echo "3" 
+ssh -t sreekar339@139.59.93.218 '
+rm ~/lol.lol
+echo "4"
+sudo rm /etc/systemd/system/GoFileServer.service
+echo "5"
+sudo mv ~/GoFileServer.service /etc/systemd/system/GoFileServer.service
+echo "6"
+sudo systemctl daemon-reload
+echo "7"
+sudo systemctl enable GoFileServer
+echo "8"
+sudo systemctl start GoFileServer
 echo "9"
-' \
-&& echo "10"
+' 
+echo "10"
 
 echo "\ndeployToRemote script ended running\n"
